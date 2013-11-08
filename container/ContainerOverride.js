343bd20e66ed1e87aa54bbdc5ce1f7fd6b84ff0c
tubo70 <tubo@weitown.com>
2013-11-08 18:09:21 +0800

    初步扩展container，增加一个allItems属性，一个冒泡的事件

--- container/ContainerOverride.js
+++ container/ContainerOverride.js
@@ -1,16 +1,30 @@
-Ext.define('Ext.container.ContainerOverride', function () {
-    return {
-        override: 'Ext.container.Container',
-        initComponent: function () {
-            var me = this;
-            me.addEvents(
-
-            );
-
-            me.callParent();
-        },
-        onAdd: function () {
-
+Ext.define('Ext.container.ContainerOverride', {
+    override: 'Ext.container.Container',
+    allItems: {},
+    initComponent: function () {
+        var me = this;
+        me.addEvents(
+            'bubblyadd'
+        );
+        me.on('bubblyadd', me.onBubblyadd, me);
+        me.callParent();
+    },
+    onAdd: function (cmp, index) {
+        var me = this;
+        me.callParent(arguments);
+        var name = cmp.itemId || cmp.name;
+        if (name) {
+            me.allItems[name] = cmp;
+            if (me.hasListeners.bubblyadd) {
+                me.fireEvent('bubblyadd', cmp, index);
+            }
+        }
+    },
+    onBubblyadd:function(cmp, index) {
+        var me = this;
+        var name = cmp.itemId || cmp.name;
+        if (name) {
+            me.allItems[name] = cmp;
         }
     }
 });
